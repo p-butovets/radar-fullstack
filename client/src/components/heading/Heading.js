72861a590d6bab211562f7fdfd949df79b3a3e5b
Butovets Pavlo <p.butovets@gmail.com>
2023-01-23 18:11:02 +0200

    brand new Header with buttons and icons

--- client/src/components/heading/Heading.js
+++ client/src/components/heading/Heading.js
@@ -1,13 +1,34 @@
+import { useState, useContext } from 'react';
+import { useNavigate } from "react-router-dom";
+import { CommonContext } from '../../context/CommonContext';
 import './heading.scss';
 
 const Heading = (props) => {
+    const context = useContext(CommonContext);
+    const navigate = useNavigate();
+
     const { title, subtitle, icon } = props;
+
+    const clickHandler = () => {
+        navigate('/admin')
+    }
+
     return (
-        <section className="heading">
-            <i className="small material-icons">{icon}</i>
-            <span className="heading_title">{title}</span>
-            <span className="heading_subtitle">{subtitle}</span>
-        </section>
+        <header className='header'>
+            <div className="header_logo">
+                <div className="header_title">
+                    <i className="small material-icons">{icon}</i>
+                    {title}
+                </div>
+                <div className="header_subtitle">{subtitle}</div>
+            </div>
+            <div className="header_buttons">
+                {context.isAdmin ?
+                    <i title="Settings" onClick={clickHandler} className="small material-icons pressable">settings</i>
+                    : null}
+                <i title="Logout" onClick={context.logout} className="small material-icons pressable">exit_to_app</i>
+            </div>
+        </header>
     )
 }
 
