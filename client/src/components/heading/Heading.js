fcb5e4becc16b464c50fd939bf4b564656791ca7
Butovets Pavlo <p.butovets@gmail.com>
2023-01-27 12:13:56 +0200

    add show user login feature

--- client/src/components/heading/Heading.js
+++ client/src/components/heading/Heading.js
@@ -17,11 +17,15 @@ const Heading = (props) => {
         <header className='header'>
             <a className="header_title" href="/">{title}</a>
             <div className="header_links">
+                <div className="userlogin blue-text text-darken-2">
+                    {context.userLogin}
+                </div>
                 {context.isAdmin ?
-                    <i title="Settings" onClick={clickHandler} className="small material-icons pressable">settings</i>
+                    <i title="Settings" onClick={clickHandler}
+                        className="material-icons pressable">settings</i>
                     : null}
                 {context.isAuthenticated ?
-                    <i title="Logout" onClick={context.logout} className="small material-icons pressable">exit_to_app</i>
+                    <i title="Logout" onClick={context.logout} className="material-icons pressable">exit_to_app</i>
                     : null}
             </div>
         </header>
