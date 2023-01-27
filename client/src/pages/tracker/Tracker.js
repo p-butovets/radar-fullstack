3c9229d41493035fe4a8822755b1b0081b041205
Butovets Pavlo <p.butovets@gmail.com>
2023-01-27 22:22:33 +0200

    add new components

--- client/src/pages/tracker/Tracker.js
+++ client/src/pages/tracker/Tracker.js
@@ -1,15 +1,45 @@
-import { useState, useContext } from 'react';
-import { CommonContext } from '../../context/CommonContext';
-import Heading from '../../components/heading/Heading';
+import { useState } from 'react';
+import { TrackerContext } from '../../context/TrackerContext';
+import useSyrve from '../../hooks/syrve.hooks';
+
+import Spinner from '../../components/spinner/Spinner';
+import Map from '../../components/map/Map';
+import ButtonGroup from '../../components/buttonGroup/ButtonGroup';
+
+import config from '../../data/common.conf.json';
 import './tracker.scss';
 
 const Tracker = () => {
 
-    const context = useContext(CommonContext)
+    const { syrveToken, organizations, loading } = useSyrve();
+
+    /*по дефолту центр карты по Киеву */
+    const [mapCenter, setMapCenter] = useState(config.DEFAULT_MAP_CENTER);
+
+    /*отображать курьеров этой организации */
+    const [showOrganizationID, setShowOrganizationID] = useState(null);
+
+
+    return (
+        <TrackerContext.Provider value={{
+            syrveToken,
+            mapCenter,
+            setMapCenter,
+            organizations,
+            showOrganizationID,
+            setShowOrganizationID
+        }}>
+            {loading ? <Spinner /> : <View />}
+        </TrackerContext.Provider>
+    )
+};
+
+const View = () => {
 
     return (
         <>
-            tracker
+            <ButtonGroup />
+            <Map />
         </>
     )
 }
