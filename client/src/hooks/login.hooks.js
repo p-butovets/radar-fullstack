6aa867ff1f333706be0f99e4107605571d88928a
Butovets Pavlo <p.butovets@gmail.com>
2023-01-27 12:15:48 +0200

    add userLogin feature

--- client/src/hooks/login.hooks.js
+++ client/src/hooks/login.hooks.js
@@ -8,18 +8,21 @@ function useLogin() {
     const [userToken, setUserToken] = useState(null);
     const [isAdmin, setIsAdmin] = useState(null);
     const [userId, setUserId] = useState(null);
+    const [userLogin, setUserLogin] = useState(null);
 
-    const login = useCallback((userToken, isAdmin, userId) => {
+    const login = useCallback((userToken, isAdmin, userId, userLogin) => {
         setUserToken(userToken);
         setIsAdmin(isAdmin);
         setUserId(userId);
-        localStorage.setItem(userData, JSON.stringify({ userToken, isAdmin, userId }))
+        setUserLogin(userLogin)
+        localStorage.setItem(userData, JSON.stringify({ userToken, isAdmin, userId, userLogin }))
     }, []);
 
     const logout = useCallback(() => {
         setUserToken(null);
         setIsAdmin(null);
         setUserId(null);
+        setUserLogin(null)
         localStorage.removeItem(userData);
         M.toast({ html: "👋😢 Logout success" });
     }, []);
@@ -27,11 +30,11 @@ function useLogin() {
     useEffect(() => {
         const data = JSON.parse(localStorage.getItem(userData));
         if (data && data.userToken) {
-            login(data.userToken, data.isAdmin, data.userId)
+            login(data.userToken, data.isAdmin, data.userId, data.userLogin)
         }
     }, [login])
 
-    return { login, logout, userToken, userId, isAdmin };
+    return { login, logout, userToken, userId, isAdmin, userLogin };
 }
 
 export default useLogin;
