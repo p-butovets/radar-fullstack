a2160c5a6d32e8ae18334bfd50dc6421eef95b24
Butovets Pavlo <p.butovets@gmail.com>
2023-01-29 00:40:41 +0200

    add new service to get courirs locations by orgID

--- client/src/services/syrveCloudService.js
+++ client/src/services/syrveCloudService.js
@@ -16,6 +16,16 @@ class SyrveCloudService extends Component {
         const response = await fetch(`/api/organizations`, requestOptions);
         return response
     }
+
+    couriers = async (token, organizationIDs) => {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ token, organizationIDs })
+        };
+        const response = await fetch(`/api/couriers`, requestOptions);
+        return response
+    }
 }
 
 export default SyrveCloudService;
