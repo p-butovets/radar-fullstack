31ab53e8dadccb904f2f82aeff89cd679a77dec5
Butovets Pavlo <p.butovets@gmail.com>
2023-01-29 00:40:09 +0200

    add new route to get couriers location fron Syrve

--- routes/organizations.routes.js
+++ routes/organizations.routes.js
@@ -3,7 +3,7 @@ const router = Router();
 require('dotenv').config();
 
 
-// /api
+// /api/organizations
 router.post('/organizations', async (req, res) => {
     try {
         if (!req.body.token) {
@@ -23,6 +23,32 @@ router.post('/organizations', async (req, res) => {
                 })
             };
             fetch(`${process.env.SYRVECLOUD_URL}organizations`, requestOptions)
+                .then((result) => result.json())
+                .then((data) => res.status(200).json(data));
+        }
+
+    } catch (err) {
+        res.status(500).json({ message: "Что-то пошло не так" })
+    }
+})
+
+// /api/couriers
+router.post('/couriers', async (req, res) => {
+    const { token, organizationIDs } = req.body;
+    try {
+        if (!token || !organizationIDs || organizationIDs.length === 0) {
+            res.status(403).json({ message: 'token and organizationIDs is required' });
+        }
+        else {
+            const requestOptions = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json;',
+                    'Authorization': 'Bearer ' + token
+                },
+                body: JSON.stringify({ "organizationIds": organizationIDs })
+            };
+            fetch(`${process.env.SYRVECLOUD_URL}employees/couriers/active_location`, requestOptions)
                 .then((result) => result.json())
                 .then((data) => res.status(200).json(data));
         }
