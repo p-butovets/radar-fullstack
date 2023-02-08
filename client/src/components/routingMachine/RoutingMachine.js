6df0f94bcad5306dcb7cdab21d07fcfc2447dd68
Butovets Pavlo <p.butovets@gmail.com>
2023-02-08 19:17:49 +0200

    show way duration on show Route is on

--- client/src/components/routingMachine/RoutingMachine.js
+++ client/src/components/routingMachine/RoutingMachine.js
@@ -1,6 +1,7 @@
 import L from "leaflet";
 import { createControlComponent } from "@react-leaflet/core";
 import "leaflet-routing-machine";
+import M from 'materialize-css';
 
 const createRoutineMachineLayer = (props) => {
 
@@ -9,6 +10,15 @@ const createRoutineMachineLayer = (props) => {
     const instance = L.Routing.control({
         waypoints: waypoints
     });
+
+    instance.on('routesfound', (e) => {
+        let routes = e.routes;
+        let summary = routes[0].summary;
+        const duration = Math.round(summary.totalTime % 3600 / 60)
+        let toastHTML = `<span style="color: white">До останньої адреси ${duration} хв.</span>`;
+        M.toast({ html: toastHTML, timeRemaining: 10000 });
+    });
+
 
     return instance;
 };
