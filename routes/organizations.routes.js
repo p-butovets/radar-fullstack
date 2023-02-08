0cd715434629e35353f964b61974cdcf77c57276
Butovets Pavlo <p.butovets@gmail.com>
2023-02-09 00:30:30 +0200

    fix today date

--- routes/organizations.routes.js
+++ routes/organizations.routes.js
@@ -2,7 +2,19 @@ const { Router } = require('express');
 const router = Router();
 require('dotenv').config();
 
-let today = new Date().toISOString().slice(0, 10);
+
+const getToday = (date) => {
+    if (!(date instanceof Date)) {
+        throw new Error('Invalid "date" argument. You must pass a date instance')
+    }
+    const year = date.getFullYear()
+    const month = String(date.getMonth() + 1).padStart(2, '0')
+    const day = String(date.getDate()).padStart(2, '0')
+    return `${year}-${month}-${day}`
+}
+
+let today = getToday(new Date());
+
 
 // /api/organizations
 router.post('/organizations', async (req, res) => {
