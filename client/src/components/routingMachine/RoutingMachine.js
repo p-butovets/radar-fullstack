4900838c58be2b8b160ab6306e1193544329b26c
Butovets Pavlo <p.butovets@gmail.com>
2023-02-08 23:05:53 +0200

    hotfix

--- client/src/components/routingMachine/RoutingMachine.js
+++ client/src/components/routingMachine/RoutingMachine.js
@@ -1,7 +1,7 @@
 import L from "leaflet";
 import { createControlComponent } from "@react-leaflet/core";
 import "leaflet-routing-machine";
-import M from 'materialize-css';
+// import M from 'materialize-css';
 
 const createRoutineMachineLayer = (props) => {
 
@@ -11,13 +11,13 @@ const createRoutineMachineLayer = (props) => {
         waypoints: waypoints
     });
 
-    instance.on('routesfound', (e) => {
-        let routes = e.routes;
-        let summary = routes[0].summary;
-        const duration = Math.round(summary.totalTime % 3600 / 60)
-        let toastHTML = `<span style="color: white">До останньої адреси ${duration} хв.</span>`;
-        M.toast({ html: toastHTML, timeRemaining: 10000 });
-    });
+    // instance.on('routesfound', (e) => {
+    //     let routes = e.routes;
+    //     let summary = routes[0].summary;
+    //     const duration = Math.round(summary.totalTime % 3600 / 60)
+    //     let toastHTML = `<span style="color: white">До останньої адреси ${duration} хв.</span>`;
+    //     M.toast({ html: toastHTML, timeRemaining: 10000 });
+    // });
 
 
     return instance;
