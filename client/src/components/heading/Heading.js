9ced5ddf796ae194c838576a345edea2436acbfc
Butovets Pavlo <p.butovets@gmail.com>
2023-04-23 00:47:20 +0300

    add navigation & improve some code

--- client/src/components/heading/Heading.js
+++ client/src/components/heading/Heading.js
@@ -2,29 +2,27 @@ import { useContext } from 'react';
 import { useNavigate } from "react-router-dom";
 import { CommonContext } from '../../context/CommonContext';
 import Logo from '../logo/Logo';
+import Navigation from '../navigation/Navigation';
 import './heading.scss';
 
 const Heading = () => {
-    const context = useContext(CommonContext);
+    const { isAdmin, isAuthenticated, userLogin, logout } = useContext(CommonContext);
     const navigate = useNavigate();
-
-    const clickHandler = () => {
-        navigate('/admin')
-    }
 
     return (
         <header className='header'>
             <Logo />
+            <Navigation />
             <div className="header_links">
-                <div className="userlogin blue-text text-darken-2">
-                    {context.userLogin}
+                <div className="userlogin ">
+                    {userLogin}
                 </div>
-                {context.isAdmin ?
-                    <i title="Settings" onClick={clickHandler}
+                {isAdmin ?
+                    <i title="Settings" onClick={() => navigate('/admin')}
                         className="material-icons pressable">settings</i>
                     : null}
-                {context.isAuthenticated ?
-                    <i title="Logout" onClick={context.logout} className="material-icons pressable">exit_to_app</i>
+                {isAuthenticated ?
+                    <i title="Logout" onClick={logout} className="material-icons pressable">exit_to_app</i>
                     : null}
             </div>
         </header>
