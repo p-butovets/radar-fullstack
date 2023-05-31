72333c7a3c549cf35a8a370b6b9e78c12d9d6ad9
Butovets Pavlo <p.butovets@gmail.com>
2023-05-31 15:32:49 +0300

    try to fix f*ing error

--- routes/organizations.routes.js
+++ routes/organizations.routes.js
@@ -19,30 +19,35 @@ router.post('/organizations', async (req, res) => {
     try {
         if (!req.body.token) {
             res.status(403).json({ message: 'token is required' });
-        }
-        else {
-            const requestOptions = {
-                method: 'POST',
+        } else {
+            const config = {
                 headers: {
-                    'Content-Type': 'application/json;',
+                    'Content-Type': 'application/json',
                     'Authorization': 'Bearer ' + req.body.token
-                },
-                body: JSON.stringify({
-                    "organizationIds": [],
-                    "returnAdditionalInfo": true,
-                    "includeDisabled": false
-                })
+                }
             };
-            console.log(`POST /organizations [${new Date().toLocaleTimeString()}]`)
-            fetch(`${process.env.SYRVECLOUD_URL}organizations`, requestOptions)
-                .then((result) => result.json())
-                .then((data) => res.status(200).json(data));
-        }
 
+            const requestData = {
+                organizationIds: [],
+                returnAdditionalInfo: true,
+                includeDisabled: false
+            };
+
+            console.log(`POST /organizations [${new Date().toLocaleTimeString()}]`);
+
+            axios.post(`${process.env.SYRVECLOUD_URL}organizations`, requestData, config)
+                .then((response) => {
+                    res.status(200).json(response.data);
+                })
+                .catch(error => {
+                    console.log("error organizations", error);
+                    res.status(500).json({ message: "Что-то пошло не так" });
+                });
+        }
     } catch (err) {
-        res.status(500).json({ message: "Что-то пошло не так" })
+        res.status(500).json({ message: "Что-то пошло не так" });
     }
-})
+});
 
 // /api/couriers
 router.post('/couriers', async (req, res) => {
@@ -50,27 +55,31 @@ router.post('/couriers', async (req, res) => {
     try {
         if (!token || !organizationIDs || organizationIDs.length === 0) {
             res.status(403).json({ message: 'token and organizationIDs is required' });
-        }
-        else {
-            const requestOptions = {
-                method: 'POST',
+        } else {
+            const config = {
                 headers: {
-                    'Content-Type': 'application/json;',
+                    'Content-Type': 'application/json',
                     'Authorization': 'Bearer ' + token
-                },
-                body: JSON.stringify({ "organizationIds": organizationIDs })
+                }
             };
-            console.log(`POST /couriers [${new Date().toLocaleTimeString()}]`)
-            fetch(`${process.env.SYRVECLOUD_URL}employees/couriers/active_location`, requestOptions)
-                .then((result) => result.json())
-                .then((data) => res.status(200).json(data))
-                .catch(error => console.log("error courier"));
-        }
 
+            const requestData = { organizationIds: organizationIDs };
+
+            console.log(`POST /couriers [${new Date().toLocaleTimeString()}]`);
+
+            axios.post(`${process.env.SYRVECLOUD_URL}employees/couriers/active_location`, requestData, config)
+                .then((response) => {
+                    res.status(200).json(response.data);
+                })
+                .catch(error => {
+                    console.log("error courier", error);
+                    res.status(500).json({ message: "Что-то пошло не так" });
+                });
+        }
     } catch (err) {
-        res.status(500).json({ message: "Что-то пошло не так" })
+        res.status(500).json({ message: "Что-то пошло не так" });
     }
-})
+});
 
 // /api/orders
 router.post('/orders', async (req, res) => {
@@ -108,8 +117,8 @@ router.post('/orders', async (req, res) => {
                     res.status(200).json(response.data);
                 })
                 .catch((error) => {
-                    res.status(200).json({ message: "Что-то пошло не так", errorDescription: error });
-                    console.log("error orders", requestData);
+                    res.status(500).json({ message: "Что-то пошло не так", errorDescription: error });
+                    console.log("error orders", error);
                 });
         }
     } catch (err) {
